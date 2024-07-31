b95b6c19962b30ea634a3b72e96fd2839ef336dd
jp-gerona <julianpetergerona@gmail.com>
2024-07-31 20:39:05 +0800

    Seperate tags into a component

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,4 @@
+// Change the date format to a more readable string format
 const formatDate = (date: Date): string => {
   const options: Intl.DateTimeFormatOptions = {
     year: "numeric",
@@ -8,4 +9,11 @@ const formatDate = (date: Date): string => {
   return new Date(date).toLocaleDateString(undefined, options);
 };
 
-export { formatDate };
+// Capitalize the first letter of a string
+const capitalizeText = (text: string): string => {
+  if (typeof text !== 'string' || text.length === 0) return text;
+
+  return text.charAt(0).toUpperCase() + text.slice(1);
+};
+
+export { formatDate, capitalizeText };
